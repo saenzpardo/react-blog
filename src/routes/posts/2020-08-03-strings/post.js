1386b1b6094eb92ce2beada435f107c9d1c138c7
saenzpardo <33372315+saenzpardo@users.noreply.github.com>
2020-09-12 21:08:41 -0500

    updated blog

---
+++ src/routes/posts/2020-08-03-strings/post.js
@@ -0,0 +1,6 @@
+export default {
+    title: `Step two-torial: Code monkeys and script kiddies`,
+    tags: ['code', 'script kiddie', 'code monkey', 'tutorial hell'],
+    spoiler: "Discussing different learning approaches to popular algorithms in different languages.",
+    getContent: () => import('./document.mdx')
+}
